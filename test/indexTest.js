d4b1fbbf609056ec455575ed0696d07d5dafdc75
emmanuel-kipngeno <immanuelkipngeno7387@gmail.com>
2023-09-15 11:31:41 +0300

    completed assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,25 +1,37 @@
-require ( './root.js' );
+require('./root.js');
 
-const { name, height, message } = require("../index.js");
+const { name, height, message } = require('../index.js');
 
-describe("what-is-a-test", () => {
-  describe("Name", () => {
+describe('what-is-a-test', () => {
+  describe('Name', () => {
     it('returns "Susan"', () => {
-      expect(name).toEqual("Susan");
+      if (name === 'Susan') {
+        // Test passes
+      } else {
+        throw new Error(`Expected 'Susan', but got '${name}'`);
+      }
     });
   });
 
-  describe("Height", () => {
-    it("is less than 40 and greater than 0", () => {
-      expect(height).toBeMoreThan(0)
-      expect(height).toBeLessThan(40);
+  describe('Height', () => {
+    it('is less than 40 and greater than 0', () => {
+      if (height > 0 && height < 40) {
+        // Test passes
+      } else {
+        throw new Error(`Expected height to be greater than 0 and less than 40, but got ${height}`);
+      }
     });
   });
 
-  describe("Message", () => {
-    it("gives the name and height", () => {
-      expect(message).toInclude(name);
-      expect(message).toInclude(height);
+  describe('Message', () => {
+    it('gives the name and height', () => {
+      if (message.includes(name) && message.includes(height.toString())) {
+        // Test passes
+      } else {
+        throw new Error(`Expected message to contain name and height, but got '${message}'`);
+      }
     });
   });
 });
+
+
